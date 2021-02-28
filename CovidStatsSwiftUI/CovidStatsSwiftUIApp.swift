4be0e36abca483fba35fc881a0bbb0d46a14dd9f
VALERA <www.valera@gmail.com>
2021-02-28 13:29:08 +0300

    Initial Commit

---
+++ CovidStatsSwiftUI/CovidStatsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidStatsSwiftUIApp.swift
+//  CovidStatsSwiftUI
+//
+//  Created by MacBro on 28.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct CovidStatsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
